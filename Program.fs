0d2791d2209ec48676bee22ccf501c5875e32e67
Adrian Bay Dorph <32704186+adriandorph@users.noreply.github.com>
2022-02-11 13:48:05 +0100

    Yellow

--- Program.fs
+++ Program.fs
@@ -95,7 +95,75 @@ let rec ack (a,b) =
     | (_,_) -> (ack (a-1,(ack (a , b-1))))
 
 //2.9
+let time f =
+  let start = System.DateTime.Now
+  let res = f ()
+  let finish = System.DateTime.Now
+  (res, finish - start)
 
+let timeArg1 f a =
+    time (fun () -> f a)
+
+//2.10
+let rec downto3 f n e =
+    match n with
+    | n when n > 0 -> downto3 f (n - 1) (f n e)
+    | _ -> e
+
+let fac n =
+    downto3 (fun x y -> x * y) n 1
+
+let range (g:(int -> 'a)) (n:int) :'a list  =
+    let f = fun x y -> y |> List.append [(g x)]
+    (downto3 f n [])
+
+//2.11
+type word = (char * int) list
+
+let hello : word = [('H',4);('E',1);('L',1);('L',1);('O',1)]
+
+type squareFun = word -> int -> int -> int
+
+//2.12
+let letterPoint (word:word) pos =
+    let _ , point = word.[pos]
+    point
+
+let singleLetterScore word pos points =
+    (letterPoint word pos) + points
+
+let doubleLetterScore word pos points  =
+    (letterPoint word pos) * 2 + points
+
+let trippleLetterScore word pos points =
+    (letterPoint word pos) * 3 + points
+
+//2.13
+let doubleWordScore _ _ points =
+    points * 2
+
+let trippleWordScore _ _ points =
+    points * 3
+
+//2.14
+let isVowel (character:char) = 
+    "aeiouAEIOU".Contains(character)
+
+let oddConsonants (word:word) _ points =
+    let f = (fun x acc -> 
+        let character, _ = word.Item (x-1)
+        if isVowel character then
+            acc
+        else
+            acc + 1
+    )
+    if (downto3 f word.Length 0) % 2 = 0 then
+        points
+    else
+        -points
+
+//2.15
+type square = (int * squareFun) list
 
 [<EntryPoint>]
 let main args =
@@ -111,4 +179,20 @@ let main args =
     printfn "2.6 implodeRev : Expected: %A Actual: %A" "ECINSI#F" (implodeRev ['F';'#';'I';'S';'N';'I';'C';'E'])
     printfn "2.7 toUpper : Expected: %A Actual: %A" "F#ISNICE" (toUpper "f#isnice")
     printfn "2.8 ack: Expected: %A Actual: %A" 57 (ack (2,27))
+    //printfn "2.9 time test with ack: %A" (time (fun () -> ack (3, 11)))
+    //printfn "2.9 timeArg1: %A" (timeArg1 ack (3, 11))
+    printfn "2.10 downto3: Expected: %A Actual: %A" 6 (downto3 (fun x y -> x + y) 3 0)
+    printfn "2.10 fac: Expected: %A Actual: %A" 24 (fac 4)
+    printfn "2.10 range: Expected: %A Actual %A" [1; 2; 6; 24] (range fac 4)
+    printfn "2.11 has no functions"
+    printfn "2.12 singleLetterScore: Expected: %A Actual: %A" 1 (singleLetterScore hello 4 0)
+    printfn "2.12 singleLetterScore: Expected: %A Actual: %A" 5 (singleLetterScore hello 4 4)
+    printfn "2.12 doubleLetterScore: Expected: %A Actual: %A" 2 (doubleLetterScore hello 4 0)
+    printfn "2.12 doubleLetterScore: Expected: %A Actual: %A" 5 (doubleLetterScore hello 4 3)
+    printfn "2.12 trippleLetterScore: Expected: %A Actual: %A" 3 (trippleLetterScore hello 4 0)
+    printfn "2.12 trippleLetterScore: Expected: %A Actual: %A" 5 (trippleLetterScore hello 4 2)
+    printfn "2.12 doubleWordScore: Expected: %A Actual: %A" 4 (doubleWordScore hello 4 2)
+    printfn "2.13 trippleWordScore: Expected: %A Actual: %A" 6 (trippleWordScore hello 4 2)
+    printfn "2.14 oddConsonants: Expected: %A Actual: %A" -8 (oddConsonants hello 0 8)
+    
     0
